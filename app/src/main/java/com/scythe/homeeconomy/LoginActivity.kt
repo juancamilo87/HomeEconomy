574432dff6786c5d570d670891f22617148a4007
Camilo <juan.camilo.garcia@hyperin.com>
2019-02-02 22:02:45 +0200

    Initial screen tutorial.

---
+++ app/src/main/java/com/scythe/homeeconomy/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.scythe.homeeconomy
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_view)
+    }
+}
